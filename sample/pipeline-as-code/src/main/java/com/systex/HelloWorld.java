1df792cdc2054eee88890c0823a96ccf77242c45
Lin Chinghui <linchinghui@gmail.com>
2019-04-25 16:32:38 +0800

    trigger for SCM changment

--- sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
+++ sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
@@ -2,6 +2,6 @@ package com.systex;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps二日訓練班!");
+        System.out.println("Hello DevOps二日訓練班 for changement!");
     }
 }
